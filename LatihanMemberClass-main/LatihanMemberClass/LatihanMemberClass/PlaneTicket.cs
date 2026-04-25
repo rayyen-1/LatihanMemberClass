35a78ca510150d24b258e7476854ba38d96efb7a
rayyen-1 <raina.ray.me@gmail.com>
2026-04-25 21:33:42 +0700

    Add files via upload

---
+++ LatihanMemberClass-main/LatihanMemberClass/LatihanMemberClass/PlaneTicket.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class PlaneTicket
+    {
+
+        private string _origin;
+        private string _destination;
+        private int _cost;
+        private string _currency;
+
+        public string Origin {
+            get
+            {
+                return _origin;
+            }
+            set 
+            { 
+                _origin = value;
+            } 
+        }
+        public string Destination {
+            get
+            {
+                return _destination;
+            }
+            set
+            {
+                _destination = value;
+            }
+        }
+        public int Cost {
+            get
+            {
+                return _cost;
+            }
+            set
+            {
+                _cost = value;
+            }
+        }
+        public string Currency {
+            get
+            {
+                return _currency;
+            }
+            set
+            {
+                _currency = value;
+            }
+        }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine("Origin: {0}", Origin);
+            Console.WriteLine("Destination: {0}", Destination);
+            Console.WriteLine("Cost: {0} ({1})", Cost, Currency);
+            if (Currency == "USD"){
+                Console.WriteLine("Convert to IDR: {0}", Cost * 15000);
+            }else if (Currency == "IDR"){
+                Console.WriteLine("Convert to USD: {0}", Cost / 15000);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LatihanMemberClass-main/LatihanMemberClass/LatihanMemberClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket ticket1 = new PlaneTicket();
+            PlaneTicket ticket2 = new PlaneTicket();
+
+            ticket1.Origin = "JOGJA";
+            ticket2.Origin = "JKT";
+            ticket1.Destination = "PKU";
+            ticket2.Destination = "PKU";
+            ticket1.Cost = 100;
+            ticket2.Cost = 75000;
+            ticket1.Currency = "USD";
+            ticket2.Currency = "IDR";
+
+            ticket1.PrintSummary();
+            ticket2.PrintSummary();
+        }
+    }
+}
